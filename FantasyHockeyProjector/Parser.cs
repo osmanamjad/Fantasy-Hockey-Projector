ec4e9df05efa9059d4684e53d7b33d1fb26a87e2
osmanamjad <osmanamjad@users.noreply.github.com>
2020-01-20 22:16:36 -0500

    fixed parsing class; added a new reference to make parsing excel files easieer

---
+++ FantasyHockeyProjector/Parser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+
+namespace FantasyHockeyProjector
+{
+    class Parser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+            ProcessWorkbook();
+        }
+
+        public static void ProcessWorkbook()
+        {
+            string file = @"C:\GitHub\Fantasy-Hockey-Projector\PlayerStats.xlsx";
+            Console.WriteLine(file);
+
+            foreach (var worksheet in Excel.Workbook.Worksheets(file))
+            {
+                foreach (var row in worksheet.Rows) 
+                {
+
+                    foreach (var cell in row.Cells)
+                    {
+                        Console.WriteLine(cell.Text);
+                    }
+                }
+            }
+        }
+    }
+}
+        
