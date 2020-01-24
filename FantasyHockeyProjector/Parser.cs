11677158e4bd46a411216a4c4cea9f2a02d0f1e3
osmanamjad <osmanamjad@users.noreply.github.com>
2020-01-23 22:30:53 -0500

    updated parser class to collect stats
    added new players and stattotals list to keep track.

--- FantasyHockeyProjector/Parser.cs
+++ FantasyHockeyProjector/Parser.cs
@@ -20,19 +20,33 @@ namespace FantasyHockeyProjector
             Console.WriteLine(file);
             int rowTracker = 0;
             int cellTracker = 0;
-            ArrayList stats = new ArrayList();
+            ArrayList statCategories = new ArrayList();
+            ArrayList players = new ArrayList();
+            ArrayList statTotals = new ArrayList();
             foreach (var worksheet in Excel.Workbook.Worksheets(file))
             {
+                rowTracker = 0;
                 foreach (var row in worksheet.Rows) 
                 {
+                    cellTracker = 0;
+                    float individualStatTotal = 0;
                     foreach (var cell in row.Cells)
                     {
-                        if (rowTracker == 0 && cellTracker >= 5)
+                        if (cellTracker == 0 && rowTracker >= 5)
                         {
-                            stats.Add(cell.Text);
+                            statCategories.Add(cell.Text);
+                        }
+                        else if(cellTracker > 0 && rowTracker >=5)
+                        {
+                            individualStatTotal += float.Parse(cell.Text);
+                        }
+                        if (rowTracker == 0 && cellTracker >= 1)
+                        {
+                            players.Add(cell.Text);
                         }
                         cellTracker++;
                     }
+                    statTotals.Add(individualStatTotal);
                     rowTracker++;
                 }
             }
